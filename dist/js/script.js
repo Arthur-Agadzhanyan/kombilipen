e492fc56ff6e47117ece35b3a267260a2817d9da
Arthur-Agadzhanyan <artur0505gold@gmail.com>
2022-02-06 15:27:05 +0300

    adapted to 320px: intro, deficit, benefits, where_to_buy sections

--- dist/js/script.js
+++ dist/js/script.js
@@ -15,3 +15,22 @@ function testWebP(callback) {
     document.querySelector('body').classList.add('no-webp');
     }
 });;
+
+
+const navbarBtn = document.getElementById("navbar__btn")
+const mobileBar = document.querySelector(".mobile_bar")
+
+navbarBtn.addEventListener('click',(e)=>{
+
+    e.currentTarget.classList.toggle('burger_active')
+
+    mobileBar.classList.toggle("mb_bar_closed")
+
+    if(mobileBar.classList.contains('bot_0')){
+        mobileBar.classList.remove("bot_0")
+    }else{
+        setTimeout(()=>{
+            mobileBar.classList.add("bot_0")
+        },500)
+    } 
+})
